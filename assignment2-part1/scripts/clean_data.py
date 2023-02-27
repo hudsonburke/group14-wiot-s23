8f44d960c1475686fb1084809aef21072d1dc27d
Hudson Burke <hpb7kr@virginia.edu>
2023-02-27 13:51:06 -0500

    Working part 1

--- assignment2-part1/scripts/clean_data.py
+++ assignment2-part1/scripts/clean_data.py
@@ -2,56 +2,66 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # Read in data
-advertisements = list[str]
-with open("./data/packets.csv", 'r') as file:
-    advertisements.append(file.readline())
+advertisements = []
+with open("./data/packets3.csv", 'r') as file:
+    advertisements = [line.rstrip() for line in file]
 
 # Remove duplicates (shouldn't be any but just in case)
-dupe_count = 0
-num_ads = len(advertisements)
-for i in range(num_ads):
-    for j in range(num_ads):
-        if advertisements[i] == advertisements[j]:
-            dupe_count += 1
-            print("Found duplicate #"+str(dupe_count))
-            del advertisements[i]
+# dupe_count = 0
+# num_ads = len(advertisements)
+# for i in range(num_ads):
+#     for j in range(num_ads):
+#         if advertisements[i] == advertisements[j]:
+#             dupe_count += 1
+#             print("Found duplicate #"+str(dupe_count))
+#             del advertisements[i]
 # OR
-unique_ads = [*set(advertisements)] # supposedly removes duplicates? https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
+advertisements = [*set(advertisements)] # supposedly removes duplicates? https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
 
 # Count
-num_elements_per_advertisement = {}
-num_advertisements_per_type = {}
+num_advertisements_with_num_elements = {} # Key = Num Elements, Value = Num advertisements w/ that num elements
+num_advertisements_per_type = {} # Key = Type, Value = Num advertisements w/ that type
 
 for ad in advertisements:
     # csv line structure: src_addr, adv_type, payload{[LEN, TYPE, DATA], ...}
-    data = ad.split() # splits on whitespace
-    num_elements = (len(data)-2)/3
-    if num_elements in num_elements_per_advertisement.keys():
-        num_elements_per_advertisement[num_elements] += 1
+    data = ad.split(',') # splits on commas
+    # num_elements = data[-1] # counted by scanner
+    num_elements = (len(data)-2)/3 
+    if num_elements in num_advertisements_with_num_elements.keys():
+        num_advertisements_with_num_elements[num_elements] += 1
     else:
-        num_elements_per_advertisement[num_elements] = 0
-    
-    types = [*set(ad[3::2])] # supposedly removes duplicates? https://www.geeksforgeeks.org/python-ways-to-remove-duplicates-from-list/
+        num_advertisements_with_num_elements[num_elements] = 1
+    types = [*set(data[3::3])] 
     for t in types:
         if t in num_advertisements_per_type.keys():
             num_advertisements_per_type[t] += 1
         else:
-            num_advertisements_per_type[t] = 0
+            num_advertisements_per_type[t] = 1
 
+print(num_advertisements_per_type)
+print(num_advertisements_with_num_elements)
 # Make histograms
-ad_type_labels = {"0x01": "Flags", "0x09": "Complete Local Name"} # fill in once we know what we need
+ad_type_labels = {
+    "0x01": "Flags", 
+    "0x09": "Complete Local Name",
+    } # fill in once we know what we need
 
-fig, (ax1, ax2) = plt.subplots(1,2, sharey=True)
+fig, (ax1, ax2) = plt.subplots(1,2)
 
 # Num advertisements v num AD elements
-ax1.bar(range(len(num_elements_per_advertisement)), num_elements_per_advertisement.values(), tick_label=num_elements_per_advertisement.keys())
+# ax1.hist(num_advertisements_with_num_elements)
+# ax1.stairs(num_elements_per_advertisement.values(), num_elements_per_advertisement.keys(), fill=True)
+ax1.bar(range(len(num_advertisements_with_num_elements)), num_advertisements_with_num_elements.values(), tick_label=list(num_advertisements_with_num_elements.keys()), align="edge", width=1.0)
 ax1.set_title("Advertisements with certain number of AD elements")
 ax1.set_xlabel('Number of AD elements') 
 ax1.set_ylabel("Number of advertisements")     
 
 # Num advertisements v AD Type
-ax2.bar(range(len(num_advertisements_per_type)), num_advertisements_per_type.values(), tick_label=num_advertisements_per_type.keys())
+# ax2.hist(num_advertisements_per_type)
+# ax2.stairs(num_advertisements_per_type.values(), num_advertisements_per_type.keys(), fill=True)
+ax2.bar(range(len(num_advertisements_per_type)), num_advertisements_per_type.values(), tick_label=list(num_advertisements_per_type.keys()), align="edge", width=1.0)
 ax2.set_title("Advertisements with certain AD type")
 ax2.set_xlabel('AD Type Specifier') 
+ax2.set_ylabel("Number of advertisements")     
 
-fig.show()
+plt.show()
--- assignment2-part1/src/main.c
+++ assignment2-part1/src/main.c
@@ -12,33 +12,34 @@ static void scan_cb(const bt_addr_le_t *addr, int8_t rssi, uint8_t adv_type,
 		    struct net_buf_simple *buf)
 {
 	char src_addr[18];
-	
     // Convert address to typical MAC address format.
 	bt_addr_le_to_str(addr, src_addr, 18);
     
     char advertiser_data[37];
     memcpy(advertiser_data, buf->data, buf->len);
-    printk("%s %d", src_addr, adv_type);
     // csv line structure: src_addr, adv_type, payload{[LEN, TYPE, DATA], ...}
+	printk("%s,%u", src_addr, adv_type);
     uint8_t ad_len = 0;
+	// int num_elements = 0;
     for (int i = 0; i < buf->len; i++){
         ad_len = advertiser_data[i];
+
         // AD Length, AD Type
-        printk(" 0x%02x 0x%02x 0x", ad_len, advertiser_data[i+1]); 
-        // AD Data
-        for (int j = i+2; j < i + ad_len; j++){
-            printk("%02x", advertiser_data[j]);
-        }
-        i+=ad_len;
+        printk(",0x%02x,0x%02x,0x", ad_len, advertiser_data[i+1]); 
+		// AD Data
+		for (int j = i+2; j < i + 1 + ad_len; j++){
+			printk("%02x", advertiser_data[j]);
+		}
+		i+=ad_len;
     }
-    printk("\n");
+	printk("\n");
 }
 
 void main(void) 
 {
 	struct bt_le_scan_param scan_param = {
 		.type       = BT_HCI_LE_SCAN_ACTIVE,
-		.options    = BT_LE_SCAN_OPT_FILTER_DUPLICATE, // previously BT_LE_SCAN_OPT_NONE,
+		.options    = BT_LE_SCAN_OPT_FILTER_DUPLICATE, // BT_LE_SCAN_OPT_NONE,
 		.interval   = 0x0010,
 		.window     = 0x0010,
 	};
